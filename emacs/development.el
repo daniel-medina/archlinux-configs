45215c8949aa2fff14d88bdc8d705470728d7758
Daniel Medina <ifthenelse42@protonmail.com>
2017-02-07 07:44:15 +0000

    updated emacs

--- emacs/development.el
+++ emacs/development.el
@@ -20,27 +20,51 @@
 ;; -------------------
 
 (defun docblock-single () (interactive)
-       (insert "/** Comment ... */"))
+       (insert "/** Comment ... */")
+       (delete-trailing-whitespace)
+       (indent-region (point-min) (point-max) nil)
+       (untabify (point-min) (point-max)))
+
 (defun docblock-multi () (interactive)
-       (insert "/**\n * Description\n *\n * Details, explanations\n*/"))
+       (insert "/**\n * Description\n *\n * Details, explanations\n*/")
+       (delete-trailing-whitespace)
+       (indent-region (point-min) (point-max) nil)
+       (untabify (point-min) (point-max)))
+
 (defun docblock-function () (interactive)
-       (insert "/**\n * Description of the function.\n * \n * @param\n * @return\n */\n"))
+       (insert "/**\n * Description of the function.\n * \n * @param\n * @return\n */\n")
+       (delete-trailing-whitespace)
+       (indent-region (point-min) (point-max) nil)
+       (untabify (point-min) (point-max)))
+
 (defun docblock-file () (interactive)
-       (insert "/**\n * Line 1 ...\n * Line 2 ...\n *\n * @category \n * @license \n * @version \n * @link \n * @author Original Author <Daniel Medina>\n */\n\n"))
+       (insert "/**\n * Line 1 ...\n * Line 2 ...\n *\n * @category \n * @license \n * @version \n * @link \n * @author Original Author <Daniel Medina>\n */\n\n")
+       (delete-trailing-whitespace)
+       (indent-region (point-min) (point-max) nil)
+       (untabify (point-min) (point-max)))
 
 ;; -------------------
 ;; Admin Intelligence Repository
 ;; -------------------
 
 (defun air-index () (interactive)
-       (insert "# ------------------------------------------------- #\n# -------------------- INDEX ---------------------- #\n# ------------------------------------------------- #"))
+       (insert "# ------------------------------------------------- #\n# -------------------- INDEX ---------------------- #\n# ------------------------------------------------- #")
+       (delete-trailing-whitespace)
+       (indent-region (point-min) (point-max) nil)
+       (untabify (point-min) (point-max)))
 
 (defun air-header () (interactive)
-       (insert "# ------------------- Name --------------------- #"))
+       (insert "# ------------------- Name --------------------- #")
+       (delete-trailing-whitespace)
+       (indent-region (point-min) (point-max) nil)
+       (untabify (point-min) (point-max)))
 
 ;; -------------------
 ;; Laravel
 ;; -------------------
 
 (defun laravel-model-scope () (interactive)
-       (insert "/**\n * Local scope\n *\n * Detail of the scope\n*/\npublic function scopeName($query)\n{\n/** Scope action ... */\n}\n"))
+       (insert "/**\n * Local scope\n *\n * Detail of the scope\n *\n * @param \Illuminate\Database\Eloquent\Builder $query\n * @return \Illuminate\Database\Eloquent\Builder\n*/\npublic function scopeName($query)\n{\n/** Scope action ... */\n}\n")
+       (delete-trailing-whitespace)
+       (indent-region (point-min) (point-max) nil)
+       (untabify (point-min) (point-max)))
